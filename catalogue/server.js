8243e81d3b19d050f0856b989ca897c49f642fcf
ashwiniethiraj3 <ashwiniethiraj@gmail.com>
2025-08-31 12:58:57 +0530

    modified server.js in catalogue

--- catalogue/server.js
+++ catalogue/server.js
@@ -17,7 +17,7 @@ try {
 }
 // --------------------------------------------------------------------------
 //-
-//
+/////
 const mongoClient = require('mongodb').MongoClient;
 const mongoObjectID = require('mongodb').ObjectID;
 const bodyParser = require('body-parser');

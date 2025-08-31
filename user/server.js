e4716ca884ca53dce7e6ce88a6a750d3574c6016
AshwiniEthiraj3 <ashwiniethiraj@gmail.com>
2025-08-31 13:46:43 +0530

    Update server.js

--- user/server.js
+++ user/server.js
@@ -11,7 +11,7 @@ try {
   console.warn(`[instana] not available, continuing without APM: ${e.message}`);
   instana = { currentSpan: () => ({ annotate: () => {} }) };
 }
-// --------------------------------------------------------------------------
+// --------//------------------------------------------------------------------
 
 const mongoClient = require('mongodb').MongoClient;
 const mongoObjectID = require('mongodb').ObjectID;

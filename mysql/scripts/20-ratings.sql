e4e4045cc193148391bd33a32e09846f25849dd3
ashwiniethiraj3 <ashwiniethiraj@gmail.com>
2025-09-08 15:41:30 +0530

    modified  dockerfile mysql

--- mysql/scripts/20-ratings.sql
+++ mysql/scripts/20-ratings.sql
@@ -1,16 +1,17 @@
-CREATE DATABASE ratings
-DEFAULT CHARACTER SET 'utf8';
+CREATE DATABASE IF NOT EXISTS ratings
+  DEFAULT CHARACTER SET utf8;
 
 USE ratings;
 
 CREATE TABLE ratings (
-    sku varchar(80) NOT NULL,
-    avg_rating DECIMAL(3, 2) NOT NULL,
+    sku VARCHAR(80) NOT NULL,
+    avg_rating DECIMAL(3,2) NOT NULL,
     rating_count INT NOT NULL,
     PRIMARY KEY (sku)
 ) ENGINE=InnoDB;
 
+-- Create the user if it doesn’t exist
+CREATE USER IF NOT EXISTS 'ratings'@'%' IDENTIFIED BY 'iloveit';
 
-GRANT ALL ON ratings.* TO 'ratings'@'%'
-IDENTIFIED BY 'iloveit';
-
+-- Grant privileges
+GRANT ALL PRIVILEGES ON ratings.* TO 'ratings'@'%';

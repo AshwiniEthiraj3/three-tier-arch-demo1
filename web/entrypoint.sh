26d7c706c9e2b7e3fb6ef54111efc905653d3d60
ashwiniethiraj3 <ashwiniethiraj@gmail.com>
2025-09-10 13:01:54 +0530

    modified ci web

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-
 BASE_DIR=/usr/share/nginx/html
 
 if [ -n "$1" ]; then
     exec "$@"
 fi
-
 # EUM disabled: create/copy eum.html safely
 echo "Instana EUM is disabled"
 if [ -f "$BASE_DIR/empty.html" ]; then
@@ -15,7 +13,6 @@ else
   echo "<!-- EUM disabled -->" > "$BASE_DIR/eum-tmpl.html"
 fi
 chmod 644 "$BASE_DIR/eum-tmpl.html"
-
 
 # Apply environment variables to default.conf
 envsubst '${CATALOGUE_HOST} ${USER_HOST} ${CART_HOST} ${SHIPPING_HOST} ${PAYMENT_HOST} ${RATINGS_HOST}' \
